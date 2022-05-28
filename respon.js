5d54fe78fae8615741c3ab1f5aaa393da360e6d7
AndrewAscar742 <andrewmatosascar@outlook.com>
2022-05-27 21:58:20 -0300

    Estágio para finalização

--- respon.js
+++
@@ -1,8 +0,0 @@
-const bntnM = document.getElementById('bntnM');
-
-function toggleMenu(){
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active')
-}
-
-bntnM.addEventListener('click',toggleMenu);
